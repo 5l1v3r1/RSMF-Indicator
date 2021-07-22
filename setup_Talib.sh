02b7e8b451281e33e222a262df9f22b03f0ceeba
Trading-Tiger <79805603+Trading-Tiger@users.noreply.github.com>
2021-07-22 15:19:31 +0200

    fix missing prefix

--- setup_Talib.sh
+++ setup_Talib.sh
@@ -1,7 +1,7 @@
 wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
 tar -xzf ta-lib-0.4.0-src.tar.gz
 cd ta-lib/
-sudo ./configure
+sudo ./configure prefix=/usr
 sudo make
 sudo make install
 python3 -m pip install ta-lib
