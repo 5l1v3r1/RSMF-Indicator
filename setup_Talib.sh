d3436b9d52cdc3f0ec7129314e0612339504a9b9
Trading-Tiger <79805603+Trading-Tiger@users.noreply.github.com>
2021-07-22 15:34:05 +0200

    fix

--- setup_Talib.sh
+++ setup_Talib.sh
@@ -1,8 +1,10 @@
 sudo apt install python3-pip build-essential -y
 wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
 tar -xzf ta-lib-0.4.0-src.tar.gz
+rm -rf ta-lib-0.4.0-src.tar.gz
 cd ta-lib/
 ./configure prefix=/usr
 sudo make
 sudo make install
-python3 -m pip install -r requirements.txt
+cd .. && rm -rf ta-lib
+python3 -m pip install -r ../requirements.txt
